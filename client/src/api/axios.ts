14b6751d95aac3b3d123c1056822c9064e5620f1
Doris Ann Augustine <dorisann.me@gmail.com>
2024-07-25 00:09:20 +0000

    --changed api url

--- client/src/api/axios.ts
+++ client/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://a-medicare-api.vercel.app/api/v1",
+  baseURL: "https://anns-medicare-api.vercel.app/api/v1",
   withCredentials: true
 });
 
